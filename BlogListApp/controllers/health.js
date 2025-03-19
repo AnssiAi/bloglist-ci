1cfc1efbc18073376b672bece1d9dbf04d7156e8
Anssi Äijö <anssi.aijo@edu.karelia.fi>
2025-03-19 12:00:01 +0200

    11.21 pullrequest required

--- BlogListApp/controllers/health.js
+++ BlogListApp/controllers/health.js
@@ -1,7 +1,7 @@
 const healthRouter = require('express').Router();
 
 healthRouter.get('/', (req, res) => {
-  res.status(200).send('Service live');
+  res.status(200).send('Service live 11.21');
 });
 
 module.exports = healthRouter;
